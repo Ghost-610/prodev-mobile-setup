43a7a1aba010f87f4a9ae4b857e7734eb1186519
elorm gershon <alidugershon@gmail.com>
2025-08-29 13:34:45 +0000

    update 2.0

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
